b02b9b7457761239dbf6cd54ffe79c82d5bf591e
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2023-10-15 22:46:06 -0400

    Remove unsed tests

--- src/app/core/pipes/sort-standings/sort-standings.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortStandingsPipe } from './sort-standings.pipe';
-
-describe('SortStandingsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortStandingsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
