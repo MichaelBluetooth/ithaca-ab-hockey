fdd5d1918f1001ac106fb13a550432e175cabe29
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2024-01-11 18:21:18 -0500

    updates 1/11

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -14,48 +14,36 @@ interface SecondaryContent {
   styleUrls: ['./secondary-content.component.less']
 })
 export class SecondaryContentComponent {
+  
   sections: SecondaryContent[] = [
     {
-      title: '12/20 - Dianes Defeat Instant Replay',
-      description: 'Goalie Rich Ford remains hot, helps Dipsticks defeat Instant Replay 6-3',
-      image: 'assets/images/dianes.png',
-      link: ''
-    },
-    {
-      title: '12/18 - Jewelers Down Dianes Dipsticks',
-      description: 'Mansour Jewelers remain hot, defeat Dianes Dipsticks 3-2',
-      image: 'assets/images/dianes_vs_mansours_12-18-2023.png',
-      link: 'https://www.youtube.com/watch?v=P9t8eheOnIk'
-    },
-    {
-      title: '12/18 - Black Sheep Streak',
-      description: 'Pineapples fall to streaking Black Sheep in Monday night rematch - lose 4-6',
-      image: 'assets/images/blacksheep_vs_orcutt_12-18-2023.png',
-      link: 'https://www.youtube.com/watch?v=PktX9a2RLUk'
-    },
-    {
-      title: '12/17 - Back in Black (Sheep)',
-      description: 'Black Sheep double down on their double header victories, defeat Orcutt Pineapples 6-4',
-      image: 'assets/images/blacksheep_vs_orcutt_12-17-2023.png',
-      link: 'https://www.youtube.com/watch?v=yvi9ZiNG_Wo'
-    },
-    {
-      title: '12/17 - E. Net: league hero',
-      description: 'E. Net comes up big for Sheep, scored lone goal in in 1-0 win against CRC Outlaws',
-      image: 'assets/images/who_is_e_net.png',
-      link: '',
-      borderedImage: true
-    },
-    {
-      title: '12/17 - Jewelers Jump Ahead',
-      description: 'Dipsticks fall to Mansour Jewelers, 2-5',
+      title: '1/9 - Mansour Jewelers Remain HOT, win 9-3',
+      description: 'Black Sheep fall to streaking Mansour Jewelers Wednesday night',
       image: 'assets/images/mansours.png',
       link: ''
     },
     {
-      title: '12/13 - Black Sheep Sharp',
-      description: 'Black Sheep defeat Mansour Jewelers 5-3',
-      image: 'assets/images/blacksheep_face.png',
+      title: '1/8 - Dianes Dipsticks Hold On, win 4-3',
+      description: 'Late flurry by Pineapples not enough to overcome strong play by Dipsticks',
+      image: 'assets/images/orcutt_vs_dianes_1-11-2024.png',
+      link: 'https://www.youtube.com/watch?v=4e9EnV35uLw'
+    },
+    {
+      title: '1/7 - Black and Blue',
+      description: 'Dianes Dipsticks defeat league leader Black Sheep 6-2',
+      image: 'assets/images/dianes_vs_blacksheep_1-7-2024.png',
+      link: 'https://www.youtube.com/watch?v=oj-bN7J05BA'
+    },
+    {
+      title: '1/7 - Orcutt Pineapples Double Header, Part 2',
+      description: 'Orcutt Pineapples, fresh off their first game of a double header fall to red hot Mansour Jewelers. Lose 6-1.',
+      image: 'assets/images/dianes.png',
+      link: ''
+    },
+    {
+      title: '1/7 - Orcutt Pineapples Double Header, Part 1',
+      description: 'Orcutt Pineapples win their first of a double header, defeat Instant Replay 3-2.',
+      image: 'assets/images/orcutt_real_estate.png',
       link: ''
     }
   ]
