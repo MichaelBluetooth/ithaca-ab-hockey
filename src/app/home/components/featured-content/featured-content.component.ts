dde0e733de485299f7823e26e732ed0168ac70f4
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2024-02-02 17:44:31 -0500

    updates

--- src/app/home/components/featured-content/featured-content.component.ts
+++ src/app/home/components/featured-content/featured-content.component.ts
@@ -11,6 +11,6 @@ export class FeaturedContentComponent {
 
   constructor(domSanitizer: DomSanitizer) {
     // this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/CYPNYEfMceg?autoplay=0');
-    this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/ehR2RmNfbzo");
+    this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/TIe-X0RIwuE");
   }
 }
--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,39 +17,36 @@ export class SecondaryContentComponent {
   
   sections: SecondaryContent[] = [
     {
-      title: '1/24 - Instant Replay Defeat Mansour Jewelers',
-      description: 'Goalies Dan Dorney and Rich Ford solid in low-scoring affair. Instant Replay win 3-1.',
-      image: 'assets/images/instant-replay.png'
+      title: "1/31 - Pineapples Squeeze By Instant Replay",
+      description: "Goalies Rich Ford and Dan Dorney strong, but Pineapples score late, win 4-3",
+      image: "assets/images/orcutt_vs_instantreplay.png"
     },
     {
-      title: '1/22 - Dianes D-d-d-d-dominate',
-      description: 'Dianes play some hockey, play great, score 8. Orcutts were there too.',
-      image: 'assets/images/dianes_vs_orcutt_1-22-2024.png',
-      link: 'https://www.youtube.com/watch?v=NjMwfCAhUso'
+      title: "1/29 - Black Sheep Bounce Back",
+      description: "Black Sheep and league leader E. Net defeat Mansour Jewelers 4-2",
+      image: "assets/images/blacksheep_face.png"
     },
     {
-      title: '1/22 - Black Sheep Showdown',
-      description: 'Hobbs and Conroy both strong as Black Sheep edge Outlaws in 4-3 victory',
-      image: 'assets/images/blacksheep_vs_outlaws_1-22-2024.png',
-      link: 'https://www.youtube.com/watch?v=WEZ6fvQpEz8'
+      title: "1/29 - CRC Outlaws Down Dianes",
+      description: "CRC Outlaws defense solid in 7-1 victory over Dianes Dipsticks",
+      image: "assets/images/outlaws_vs_dianes_1-29-2024.png",
+      link: 'https://www.youtube.com/watch?v=XGIJWt1AYdQ'
     },
     {
-      title: '1/21 - E. Net leads Orcutt in 1-0 win',
-      description: 'E. Net, projected league MVP, scores yet ANOTHER game winning goal',
-      image: 'assets/images/who_is_e_net.png',
-      borderedImage: true
+      title: "1/28 - Dianes Deliver Decisive Defeat",
+      description: "Dianes defense solid in 6-1 victory over Black Sheep",
+      image: "assets/images/dianes_vs_blacksheep_1-28-2024.png",
+      link: "https://www.youtube.com/watch?v=fhy1Mo1jk9U"
     },
     {
-      title: '1/21 - Black Sheep defeat Instant Replay',
-      description: 'Goalie Alex Chang solid in 4-1 win',
-      image: 'assets/images/blacksheep_face.png',
-      link: ''
+      title: "1/28 - Instant Replay and CRC Outlaws Evenly Matched",
+      description: "Goalies Alex Chang and Thomas Conroy both strong in 4-4 tie game",
+      image: "assets/images/outlaws.png"
     },
     {
-      title: '1/21 - Mansour 3, Dianes 2',
-      description: 'Goaltender Evan Mielbrecht makes season debut with a win',
-      image: 'assets/images/mansours.png',
-      link: ''
+      title: "1/28 - Pineapples Pulverize Mansour",
+      description: "Orcutt Pineapples score 8, defeat Mansour Jewelers",
+      image: "assets/sponsors/orcutt_real_estate.jpg"
     },
   ]
 }
