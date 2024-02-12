627bda4297b2cc6b55605d142cd7527ba740d6e6
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2024-02-12 17:43:41 -0500

    update pre winter break

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -14,39 +14,39 @@ interface SecondaryContent {
   styleUrls: ['./secondary-content.component.less']
 })
 export class SecondaryContentComponent {
-  
+
   sections: SecondaryContent[] = [
     {
-      title: "1/31 - Pineapples Squeeze By Instant Replay",
-      description: "Goalies Rich Ford and Dan Dorney strong, but Pineapples score late, win 4-3",
-      image: "assets/images/orcutt_vs_instantreplay.png"
-    },
-    {
-      title: "1/29 - Black Sheep Bounce Back",
-      description: "Black Sheep and league leader E. Net defeat Mansour Jewelers 4-2",
-      image: "assets/images/blacksheep_face.png"
-    },
-    {
-      title: "1/29 - CRC Outlaws Down Dianes",
-      description: "CRC Outlaws defense solid in 7-1 victory over Dianes Dipsticks",
-      image: "assets/images/outlaws_vs_dianes_1-29-2024.png",
-      link: 'https://www.youtube.com/watch?v=XGIJWt1AYdQ'
-    },
-    {
-      title: "1/28 - Dianes Deliver Decisive Defeat",
-      description: "Dianes defense solid in 6-1 victory over Black Sheep",
-      image: "assets/images/dianes_vs_blacksheep_1-28-2024.png",
-      link: "https://www.youtube.com/watch?v=fhy1Mo1jk9U"
-    },
-    {
-      title: "1/28 - Instant Replay and CRC Outlaws Evenly Matched",
-      description: "Goalies Alex Chang and Thomas Conroy both strong in 4-4 tie game",
+      title: "2/7 - Outlaws Can't Be Stopped",
+      description: "League leaders Black Sheep can't stop red-hot CRC Outlaws, fall 3-8",
       image: "assets/images/outlaws.png"
     },
     {
-      title: "1/28 - Pineapples Pulverize Mansour",
-      description: "Orcutt Pineapples score 8, defeat Mansour Jewelers",
+      title: "2/5 - Instant Replay Wins",
+      description: "In rare low-scoring event, Instant Replay overcome Dianes Dipsticks 2-1",
+      image: "assets/images/instant-replay.png"
+    },
+    {
+      title: "2/5 - Outlaws Remain Hot!",
+      description: "CRC Outlaws sneak by Mansour Jewelers in 4-3 victory",
+      image: "assets/images/outlaws_vs_mansour.png",
+      link: "https://www.youtube.com/watch?v=1hwUx7HloVo"
+    },
+    {
+      title: "2/4 - Mansours Down Dianes",
+      description: "Mansour Jewelers defeat Dianes Dipsticks 5-2",
+      image: 'assets/images/mansour_vs_dianes_2-4-2024.png',
+      link: "https://www.youtube.com/watch?v=xbRiBbCg8lk"
+    },
+    {
+      title: "2/4 - Orcutt Pineapples Taught a Lesson",
+      description: "MBA Outlaws down Orcutt Pineapples, winning 4-2",
       image: "assets/sponsors/orcutt_real_estate.jpg"
     },
+    {
+      title: "2/4 - Black Sheep down Instant Replay",
+      description: "Goalies Mielbrecht and Pace keep it close, but Black Sheep overcome Instant Replay 4-2",
+      image: "assets/images/blacksheep_face.png"
+    }
   ]
 }
