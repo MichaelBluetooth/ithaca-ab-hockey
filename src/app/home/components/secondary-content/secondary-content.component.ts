24805c1c8aed112850241391862e0469c4a739c9
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2024-05-06 21:44:38 -0400

    2024 pre-season updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,36 +17,16 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [
     {
-      title: "2/7 - Outlaws Can't Be Stopped",
-      description: "League leaders Black Sheep can't stop red-hot CRC Outlaws, fall 3-8",
-      image: "assets/images/outlaws.png"
+      title: "Outlaws WIN IT ALL",
+      description: "Congratulations to the MBA team and goaltender Rich Ford!",
+      image: "assets/images/outlaws.png",
+      link: "https://www.youtube.com/watch?v=u8vVIxWCWNc"
     },
     {
-      title: "2/5 - Instant Replay Wins",
-      description: "In rare low-scoring event, Instant Replay overcome Dianes Dipsticks 2-1",
-      image: "assets/images/instant-replay.png"
-    },
-    {
-      title: "2/5 - Outlaws Remain Hot!",
-      description: "CRC Outlaws sneak by Mansour Jewelers in 4-3 victory",
-      image: "assets/images/outlaws_vs_mansour.png",
-      link: "https://www.youtube.com/watch?v=1hwUx7HloVo"
-    },
-    {
-      title: "2/4 - Mansours Down Dianes",
-      description: "Mansour Jewelers defeat Dianes Dipsticks 5-2",
-      image: 'assets/images/mansour_vs_dianes_2-4-2024.png',
-      link: "https://www.youtube.com/watch?v=xbRiBbCg8lk"
-    },
-    {
-      title: "2/4 - Orcutt Pineapples Taught a Lesson",
-      description: "MBA Outlaws down Orcutt Pineapples, winning 4-2",
-      image: "assets/sponsors/orcutt_real_estate.jpg"
-    },
-    {
-      title: "2/4 - Black Sheep down Instant Replay",
-      description: "Goalies Mielbrecht and Pace keep it close, but Black Sheep overcome Instant Replay 4-2",
-      image: "assets/images/blacksheep_face.png"
+      title: "CRC Plays of the Year",
+      description: "Check out the best plays from the 2023 season right here!",
+      image: "assets/images/lfg_presents_2023.png",
+      link: "https://www.youtube.com/watch?v=3a6thDIr3Es"
     }
   ]
 }
