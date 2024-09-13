78bdc977885549bbf4cf179c7f2a8ac92a8be8c1
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2024-09-13 14:23:05 -0400

    18 days

--- src/app/home/components/featured-content/featured-content.component.ts
+++ src/app/home/components/featured-content/featured-content.component.ts
@@ -11,6 +11,6 @@ export class FeaturedContentComponent {
 
   constructor(domSanitizer: DomSanitizer) {
     // this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/CYPNYEfMceg?autoplay=0');
-    this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/OXq4LxxTFr8");
+    this.videoUrl = domSanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/cYklJZyM4lc");
   }
 }
