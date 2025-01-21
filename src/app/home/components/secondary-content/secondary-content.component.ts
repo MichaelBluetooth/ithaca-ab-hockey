2a753b85ff802c77fa016e2d02e6b6ac702773d0
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-01-21 14:34:50 -0500

    weekly updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,34 +17,28 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [    
     {
-      title: "1/15 - Mansour Stay On Top",
-      description: "Mansour overcome Dianes in close game, remain top dog, win 4-3",
-      image: "assets/images/mansours.png",
+      title: "1/20 - Black Sheep Schooled by MBAs",
+      description: "Fresh legged MBAs stay hot, defeat Black Sheep 12-3",
+      image: "assets/images/blacksheep_vs_mbas.png",
       link: ""
     },
     {
-      title: "1/13 - Hot Hot...Ice Cream?",
-      description: "Ice Cream Bar win again, defeat Orcutt Real Estate 7-3",
-      image: "assets/sponsors/ice_cream_bar.jpg",
+      title: "1/19 - Dianes Defeat Mansour",
+      description: "Dianes Dipsticks topple Mansour Jewelers, win 6-4",
+      image: "assets/images/dianes_vs_mansour_11-3-2024.PNG",
       link: ""
     },
     {
-      title: "1/12 - Orcutt Shear Sheep",
-      description: "Orcutt Real Estate defeat Black Sheep 6-3",
-      image: "assets/images/orcutt_vs_blacksheep_12-16-2024.png",
+      title: "1/19 - Black Sheep and Ice Cream Bar",
+      description: "Black Sheep and Ice Cream Bar end game in a tie, 5-5",
+      image: "assets/images/blacksheep_vs_icecreambar_12-15-2024.jpg",
       link: ""
     },
     {
-      title: "1/12 - I Scream for Ice Cream",
-      description: "Ice Cream Bar defeat Dianes with late goal, win 3-2",
-      image: "assets/images/dianes_vs_icecreambar_1-12-2025.png",
-      link: "https://www.youtube.com/watch?v=2KC8sbN3vvU"
-    },
-    {
-      title: "1/12 - Mansour Melt Ice Cream Bar",
-      description: "Ice Cream Bar dealt loss by Mansour in first of back-to-back games, 5-3",
-      image: "assets/images/mansour_vs_icecreambar_1-13-2025.jpg",
-      link: "https://www.youtube.com/watch?v=qcUPa9RaSTY"
+      title: "1/19 - Orcutt Taught Lesson By MBAs",
+      description: "MBAs, fresh from break, restart season strong, win 5-3",
+      image: "assets/images/instantreplay_vs_orcutt_12-11-2023.png",
+      link: ""
     }
   ]
 }
