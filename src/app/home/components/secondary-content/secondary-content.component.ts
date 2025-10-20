d6e4e1b3d1574e4b6ced2c8eb659741bc1fdea52
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-10-20 15:05:48 -0400

    updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,40 +17,22 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [    
     {
-      title: "10/16 - Orcutt Can't Solve Mansour",
-      description: "Orcutt Real Estate improve, but still can't defeat Mansour Jewelers. Fall 6-3",
-      image: "assets/images/2025-2026/Mansour_vs_Orcutt_2.png",
+      title: "10/20 - Ice Cream Bar Freezes Out DSP Shows",
+      description: "Ice Cream Bar confident in 8-3 victory, keep DSP Shows on ice from start to finish.",
+      image: "assets/images/2025-2026/ICB_vs_DSP.png",
       link: ""
     },
     {
-      title: "10/13 - Ice Cream Bar Ices DSP Shows",
-      description: "Ice Cream Bar secures first win against DSP Shows, win 7-1",
-      image: "assets/images/2025-2026/ICV_vs_DSP.png",
-      link: ""
-    },
-    {
-      title: "10/12 - Dianes Dipsticks defeat Ice Cream Bar",
-      description: "Dianes continue win streak, win 8-5",
-      image: "assets/images/2025-2026/ICB_vs_Dianes.png",
-      link: ""
-    },
-    {
-      title: "10/12 - Mansour Jewelers Manhandle Orcutt Real Estate",
-      description: "New kits proove effective, win decisively 11-2",
-      image: "assets/images/2025-2026/Mansour_vs_Orcutt.png",
-      link: ""
-    },
-    {
-      title: "10/12 - DSP Shows get first win over Orcutt Real Estate",
-      description: "Orcutt kept it close, but DSP wins 5-4",
-      image: "assets/images/2025-2026/Orcutt_vs_DSP.png",
-      link: ""
-    },
-    {
-      title: "10/8 - Dianes Deal Reigning Champs First Loss",
-      description: "Dianes Dipsticks defeat MBAs 704",
-      image: "assets/images/2025-2026/Dianes_vs_DSP.png",
+      title: "10/20 - Orcutts Win Drought Still Frozen After 7-8 Loss",
+      description: "Instant Replay defeat winless Orcutt Real Estate in high scoring affair",
+      image: "assets/images/2025-2026/Orcutt_vs_InstantReplay.png",
       link: ""
     },    
+    {
+      title: "10/20 - Diane's Dipsticks Stay Polished in 8-5 Win",
+      description: "The undefeated Dipsticks kept their shine, handing Mansour Jewelers their first loss of the season in a hard-fought 8-5 matchup.",
+      image: "assets/images/2025-2026/Mansour_vs_Dianes.png",
+      link: ""
+    },
   ]
 }
