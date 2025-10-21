e6f51b0671c095a6da423a7919a37aa5975ef269
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-10-21 10:42:34 -0400

    updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,19 +17,25 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [    
     {
-      title: "10/20 - Ice Cream Bar Freezes Out DSP Shows",
+      title: "10/20 - Dianes Dipsticks Win Streak Continues",
+      description: "Dianes dominate the ice in convincing 9-3 win over Instant Replay Sports",
+      image: "assets/images/2025-2026/InstantReplay_vs_Dianes.png",
+      link: ""
+    },
+    {
+      title: "10/19 - Ice Cream Bar Freezes Out DSP Shows",
       description: "Ice Cream Bar confident in 8-3 victory, keep DSP Shows on ice from start to finish.",
       image: "assets/images/2025-2026/ICB_vs_DSP.png",
       link: ""
     },
     {
-      title: "10/20 - Orcutts Win Drought Still Frozen After 7-8 Loss",
+      title: "10/19 - Orcutts Win Drought Still Frozen After 7-8 Loss",
       description: "Instant Replay defeat winless Orcutt Real Estate in high scoring affair",
       image: "assets/images/2025-2026/Orcutt_vs_InstantReplay.png",
       link: ""
     },    
     {
-      title: "10/20 - Diane's Dipsticks Stay Polished in 8-5 Win",
+      title: "10/19 - Diane's Dipsticks Stay Polished in 8-5 Win",
       description: "The undefeated Dipsticks kept their shine, handing Mansour Jewelers their first loss of the season in a hard-fought 8-5 matchup.",
       image: "assets/images/2025-2026/Mansour_vs_Dianes.png",
       link: ""
