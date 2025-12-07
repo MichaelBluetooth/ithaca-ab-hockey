ecba98a6f1bc25897e5051906e1489232f150dd6
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-12-07 14:26:28 -0500

    updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,34 +17,16 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [
     {
-      title: "11/12 - Dianes Score More",
-      description: "Dianes Dipsticks out-score Instant Replay, win 11-6",
-      image: "assets/images/2025-2026/InstantReplay_vs_Dianes.png",
+      title: "12/1 - MBAs melt Ice Cream Bar",
+      description: "E. Net strikes again, as MBAs defeat Ice Cream Bar 7-4",
+      image: "assets/images/instantreplay_vs_icecreambar_10.27.2024.PNG",
       link: ""
     },
     {
-      title: "11/10 - Mansour Keeps Scoring",
-      description: "Mansour Jewelers victorious against DSP Shows, 9-3",
-      image: "assets/images/2025-2026/Mansour_vs_DSP_1.png",
+      title: "12/3 - MBAs win again",
+      description: "Orcutt, still looking for a win decimated by MBAs 12-4",
+      image: "assets/images/orcutt_vs_instantreplay_11-3-2024.png",
       link: ""
-    },
-    {
-      title: "11/9 - Orcutt Stops The Bleeding",
-      description: "Orcutt Real Estate stave off 9th straight loss, tie Instant Replay 6-6",
-      image: "assets/images/2025-2026/Orcutt_vs_InstantReplay.png",
-      link: ""
-    },
-    {
-      title: "11/9 - Mansour Jewelers Melt ICB",
-      description: "Mansour Jewelers defeat Ice Cream Bar, 5-2",
-      image: "assets/images/2025-2026/ICB_vs_Mansour_1.PNG",
-      link: "https://www.youtube.com/watch?v=YUL4ssHrUe0"
-    },
-    {
-      title: "11/9 - Dianes Hold On",
-      description: "Dianes Dipsticks hold off DSP Shows in high-scoring affair, win 10-9",
-      image: "assets/images/2025-2026/Dianes_vs_DSP.png",
-      link: ""
-    }
+    }    
   ]
 }
