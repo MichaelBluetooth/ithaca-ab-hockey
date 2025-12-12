0e73c755450542478776c96bb9e0f119f81a1db8
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2025-12-12 14:50:08 -0500

    updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,15 +17,27 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [
     {
-      title: "12/1 - MBAs melt Ice Cream Bar",
-      description: "E. Net strikes again, as MBAs defeat Ice Cream Bar 7-4",
-      image: "assets/images/instantreplay_vs_icecreambar_10.27.2024.PNG",
+      title: "12/8 - Orcut Win Streak Over",
+      description: "Orcutt, fresh off first win, fall to Ice Cream Bar, 2-6",
+      image: "/assets/images/orcutt_vs_dianes_10-16-2023.png",
       link: ""
     },
     {
-      title: "12/3 - MBAs win again",
-      description: "Orcutt, still looking for a win decimated by MBAs 12-4",
-      image: "assets/images/orcutt_vs_instantreplay_11-3-2024.png",
+      title: "12/7 - Orcutt Streak Just Getting Started",
+      description: "Orcutt secure first win of the season over DSP Shows, win 4-3",
+      image: "assets/images/2025-2026/Orcutt_vs_DSP.png",
+      link: ""
+    },
+    {
+      title: "12/7 - Dianes Take The Lead",
+      description: "Dianes Dipstick emerge at top contenders, defeat Mansour 7-4",
+      image: "/assets/images/2025-2026/Mansour_vs_Dianes_12.7.2025.png",
+      link: "https://www.youtube.com/watch?v=mqVHoCayFl0"
+    },
+    {
+      title: "12/7 - MBAs melt Ice Cream Bar",
+      description: "MBAs dominate game, win 12-0 against Ice Cream Bar",
+      image: "assets/images/2025-2026/InstantReplay_vs_ICB.png",
       link: ""
     }    
   ]
