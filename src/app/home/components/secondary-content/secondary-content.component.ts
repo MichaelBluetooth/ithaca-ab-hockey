299b8c5e5dda452e90988741c9c2afe55ca261c8
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2026-03-06 14:16:21 -0500

    updates

--- src/app/home/components/secondary-content/secondary-content.component.ts
+++ src/app/home/components/secondary-content/secondary-content.component.ts
@@ -17,22 +17,28 @@ export class SecondaryContentComponent {
 
   sections: SecondaryContent[] = [
     {
-      title: "HIGH SCORE!?",
-      description: "2/11 - Instant Replay tops Orcutt Real Estate in high scoring game, 12-19",
-      image: "/assets/images/2025-2026/Orcutt_vs_InstantReplay.png",
+      title: "Dianes Dipsticks take on MBAs",
+      description: "#1 seed Dianes Dipsticks take on MBAs in semi-finals",
+      image: "/assets/images/dianes_vs_mansours_12-18-2023.png",
       link: ""
     },
     {
-      title: "Goalie Duel",
-      description: "2/9 - DSP and Mansour tie 2-2 as goalies Hobbs and Pace refuse to lose",
-      image: "/assets/images/2025-2026/Mansour_vs_DSP.png",
-      link: "https://www.youtube.com/watch?v=mHquywQ-V_A"
-    },
-    {
-      title: "MBAs School DSP",
-      description: "2/4 - Instant Replay defeats DSP shows 8-4",
-      image: "/assets/images/2025-2026/InstantReplay_vs_DSP.png",
+      title: "Mansour Jewelers look to crush ICB",
+      description: "#2 seed Mansour Jewelers take on ICB in semi-finals",
+      image: "/assets/images/mansour_vs_icecreambar_1-13-2025.jpg",
       link: ""
     },
+    {
+      title: "Orcutt Eliminated",
+      description: "Ice Cream Bar eliminates Orcutt Real Estate",
+      image: "/assets/images/2025-2026/Orcutt_vs_ICB.PNG",
+      link: "https://www.youtube.com/watch?v=Ip039c3nS80"
+    },
+    {
+      title: "DSP Eliminated",
+      description: "DSP Shows takes it to overtime, but MBAs come out on top",
+      image: "/assets/images/dianes_vs_blacksheep_1-7-2024.png",
+      link: ""
+    }
   ]
 }
